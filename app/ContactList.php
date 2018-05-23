35f8e5eb5aff80a93324db93d4169c9429e4d73c
Dev3 <shayansolutions@gmail.com>
2018-05-23 12:48:41 +0500

    add create list api

--- app/ContactList.php
+++ app/ContactList.php
@@ -25,7 +25,12 @@ class ContactList extends Model
     
     public static function CreateList($request){
         $request = $request->all();
-        return self::create($request)->id;
-        
+        $user_id = $request['user_id'];
+        $user_list = self::where('user_id',$user_id)->first();
+        if($user_list){
+         return  self::where('user_id',$user_id)->update(['contact_list'=>$request['contact_list']]);
+        }else{
+            return self::create($request)->id;           
+        }
     }
 }

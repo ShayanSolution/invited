800b53d4211c835a5a38a067f41e67778e788464
Dev3 <shayansolutions@gmail.com>
2018-06-07 19:46:31 +0500

    fix delete event api issue

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -383,37 +383,32 @@ class EventController extends Controller
         $event_detail = Event::getEventByID($request['event_id']);
         $event_list_id = $event_detail->list_id;
         $notification_usres_list = ContactList::getUserList($event_list_id);
+        foreach (json_decode($notification_usres_list->contact_list) as $list){
+            $notification_user = User::where('phone',$list->phone)->first();
+            $user_device_token = $notification_user->device_token;
+            if(!empty($user_device_token)){
+                $message = PushNotification::Message($event_detail->title."  has been cancelled. "  ,array(
+                    'badge' => 1,
+                    'sound' => 'example.aiff',
 
+                    'actionLocKey' => 'Action button title!',
+                    'locKey' => 'localized key',
+                    'locArgs' => array(
+                        'localized args',
+                        'localized args',
+                    ),
+                    'launchImage' => 'image.jpg',
+
+                    'custom' => array('custom_data' => array(
+                        'event_id' => $event_detail->id,
+                        'status' => 'cancelled'
+                    ))
+                ));
+                PushNotification::app('invitedIOS')->to($user_device_token)->send($message);
+            }
+        }
         $event =Event::deleteEvent($request);
         if($event){
-
-            foreach (json_decode($notification_usres_list->contact_list) as $list){
-                $notification_user = User::where('phone',$list->phone)->first();
-                dd($notification_user);
-                $user_device_token = $notification_user->device_token;
-                if(!empty($user_device_token)){
-                    $message = PushNotification::Message($event_detail->title."  has been cancelled. "  ,array(
-                        'badge' => 1,
-                        'sound' => 'example.aiff',
-
-                        'actionLocKey' => 'Action button title!',
-                        'locKey' => 'localized key',
-                        'locArgs' => array(
-                            'localized args',
-                            'localized args',
-                        ),
-                        'launchImage' => 'image.jpg',
-
-                        'custom' => array('custom_data' => array(
-                            'event_id' => $event_detail->id,
-                            'status' => 'cancelled'
-                        ))
-                    ));
-                    PushNotification::app('invitedIOS')->to($user_device_token)->send($message);
-                }
-            }
-
-
             return response()->json(
                 [
                     'message' => 'Event Deleted Successfully',

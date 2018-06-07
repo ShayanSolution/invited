3515808812ead2f75d17cdfa80954823bf7be055
Dev3 <shayansolutions@gmail.com>
2018-06-07 12:38:47 +0500

    add lat and lng in get user request

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -206,7 +206,7 @@ class EventController extends Controller
         $accepted = RequestsEvent::acceptRequest($event_id,$id);
         if($accepted){
             $created_by = RequestsEvent::createdByRequest($event_id,$id);
-            $this->sendRequestNotification($created_by->created_by,$event_id,$request_status = "accepted");
+            $this->sendRequestNotification($id,$event_id,$request_status = "accepted");
             return response()->json(
                 [
                     'status' => 'Request accepted successfully',

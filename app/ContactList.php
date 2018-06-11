fb896bb8d4e76908f9c8e8cdc5bb23bd2f560268
Dev3 <shayansolutions@gmail.com>
2018-06-11 19:45:57 +0500

    clean phone number

--- app/ContactList.php
+++ app/ContactList.php
@@ -35,7 +35,7 @@ class ContactList extends Model
         foreach($contactList as $key => $contact){
             Log::info("cleaning phone number =>".$contact->phone);
             $contact->phone = preg_replace('/\s+/', '', trim($contact->phone));
-            $contact->phone = preg_replace('/^92|^092/', '', trim($contact->phone));
+            $contact->phone = preg_replace('/^92|^092|^\+92/', '', trim($contact->phone));
             $phone = $contact->phone;
             if($phone[0]!=0){
                 $phone='0'.$phone;

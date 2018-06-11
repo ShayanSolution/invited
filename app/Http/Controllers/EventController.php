d214d2cbe77736acb8314c639c3130619cbd89f7
Dev3 <shayansolutions@gmail.com>
2018-06-11 12:31:35 +0500

    debug

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -217,7 +217,7 @@ class EventController extends Controller
         if($accepted){
             $created_by = RequestsEvent::createdByRequest($event_id,$id);
             $accepted_user = User::where('id',$id)->first();
-            
+
             $this->sendRequestNotification($created_by->created_by,$event_id,$accepted_user,$request_status = "accepted");
             return response()->json(
                 [
@@ -453,7 +453,7 @@ class EventController extends Controller
     }
     
     public function sendNotificationToAndoidUsers($device_token){
-
+dd($device_token);
         $optionBuilder = new OptionsBuilder();
         $optionBuilder->setTimeToLive(60*20);
 

c4dbbfc87e83022b28cfea9738e572a81d6e2161
Dev3 <shayansolutions@gmail.com>
2018-06-13 14:06:38 +0500

    debug

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -260,31 +260,29 @@ class EventController extends Controller
                     if($user){
                         $device_token = $user->device_token;
                         $platform = $user->platform;
-
-                        //send notification to ios user list
-                        Log::info("Request Cycle with Queues Begins");
-                        $job = new SendCloseEventNotification($device_token, $event_detail->title);
-                        dispatch($job);
-                        Log::info('Request Cycle with Queues Ends');
-                        
                         
                         Log::info("device_token: ".$device_token);
                         if($platform == 'ios' || is_null($platform)) {
-                            $message = PushNotification::Message(" $event_detail->title has been closed ", array(
-                                'badge' => 1,
-                                'sound' => 'example.aiff',
-
-                                'actionLocKey' => 'Action button title!',
-                                'locKey' => 'localized key',
-                                'locArgs' => array(
-                                    'localized args',
-                                    'localized args',
-                                ),
-                                'launchImage' => 'image.jpg',
-
-
-                            ));
-                            PushNotification::app('invitedIOS')->to($device_token)->send($message);
+                            //send notification to ios user list
+                            Log::info("Request Cycle with Queues Begins");
+                            $job = new SendCloseEventNotification($device_token, $event_detail->title);
+                            dispatch($job);
+                            Log::info('Request Cycle with Queues Ends');
+//                            $message = PushNotification::Message(" $event_detail->title has been closed ", array(
+//                                'badge' => 1,
+//                                'sound' => 'example.aiff',
+//
+//                                'actionLocKey' => 'Action button title!',
+//                                'locKey' => 'localized key',
+//                                'locArgs' => array(
+//                                    'localized args',
+//                                    'localized args',
+//                                ),
+//                                'launchImage' => 'image.jpg',
+//
+//
+//                            ));
+//                            PushNotification::app('invitedIOS')->to($device_token)->send($message);
                         }else{
 
                             Log::info(" Send notification to android users ");

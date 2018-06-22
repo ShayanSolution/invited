f324a87054d45e20e35038584bacb1f3ea767e20
Dev3 <shayansolutions@gmail.com>
2018-06-22 16:38:07 +0500

    sending data messages in background

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -143,17 +143,8 @@ class EventController extends Controller
                                     }
                                     $event = Event::where('id',$event_id)->first();
                                     $message_title = $user_name.' '.$message.' '. $event->title.'.';
-
-                                    Log::info('Send data message in background');
-                                    $optionBuiler = new OptionsBuilder();
-                                    $optionBuiler->setTimeToLive(60*20);
-                                    $dataBuilder = new PayloadDataBuilder();
-                                    $dataBuilder->addData(['a_data' => 'my_test_data']);
-                                    $data = $dataBuilder->build();
-                                    $option = $optionBuiler->build();
-                                    FCM::sendTo($device_token, $option, null, $data);
-                                    Log::info('Now sending data message and notification in forground');
-
+                                    //send data message payload
+                                    $this->sendCreateEventDataMessage($device_token);
                                     $this->sendNotificationToAndoidUsers($device_token,$request_status,$message_title);
 
                             }
@@ -171,6 +162,18 @@ class EventController extends Controller
         {
             Log::info("I am getting empty user list");
         }
+    }
+
+    public function sendCreateEventDataMessage($device_token){
+        Log::info('Send data message in background');
+        $optionBuiler = new OptionsBuilder();
+        $optionBuiler->setTimeToLive(60*20);
+        $dataBuilder = new PayloadDataBuilder();
+        $dataBuilder->addData(['a_data' => 'my_test_data']);
+        $data = $dataBuilder->build();
+        $option = $optionBuiler->build();
+        FCM::sendTo($device_token, $option, null, $data);
+        Log::info('Now sending data message and notification in forground');
     }
 
     public function getUserEvents(Request $request){

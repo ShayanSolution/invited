1785bc3554839d3e3ede80f38629e26a94d30de8
Dev3 <shayansolutions@gmail.com>
2018-07-04 13:14:58 +0500

    Recieved Request API Issue

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -401,7 +401,8 @@ class EventController extends Controller
 
         if($requests){
             foreach($requests as $request){
-                $request->event_contact_list = json_decode($request->contact_list);
+                $request->contact_list = json_decode($request->contact_list);
+                $request->contact_list_count = count($request->contact_list);
             }
             Log::info("Received Requests =>".print_r($requests,true));
             return response()->json(

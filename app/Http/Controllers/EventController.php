fd6b3dc99772c364f0eeb56c8395f6a0766b35e4
Dev3 <shayansolutions@gmail.com>
2018-07-17 13:05:15 +0500

    event accepted android notification body test updated

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -278,7 +278,7 @@ class EventController extends Controller
         if($accepted['update']){
             $created_by = RequestsEvent::createdByRequest($event_id,$id);
             $accepted_user = User::where('id',$id)->first();
-            $this->sendRequestNotification($created_by->created_by,$event_id,$accepted_user,$request_status = "confirmed");
+            $this->sendRequestNotification($created_by->created_by,$event_id,$accepted_user,$request_status = "accepted");
             Log::info("Notification users ids for closed events: ".print_r($accepted['notification_users'],true));
             //if event has been closed, send notification remaining users for closed events
             if(!empty($accepted['notification_users'])){
@@ -384,6 +384,7 @@ class EventController extends Controller
                     PushNotification::app('invitedIOS')->to($notification_user->device_token)->send($message);
                 }
                 else{
+
                     $this->sendNotificationToAndoidUsers($notification_user->device_token,$request_status,$user_name);
                 }
             }
@@ -608,13 +609,13 @@ class EventController extends Controller
         if($request_status == 'accepted'){
             //$notificationBuilder = new PayloadNotificationBuilder('Accepted');
             //$notificationBuilder->setBody($user_name.' accepted your request')->setSound('default');
-            $dataBuilder->addData(['code' => '3','Title' => 'Accepted','Body' => $user_name.' accepted your request']);
+            $dataBuilder->addData(['code' => '3','Title' => 'Accepted','Body' => $user_name.' accepted your request.']);
             Log::info("Event Accepted:");
         }
         elseif($request_status == 'rejected'){
             //$notificationBuilder = new PayloadNotificationBuilder('Canceled');
             //$notificationBuilder->setBody($user_name.' canceled your request')->setSound('default');
-            $dataBuilder->addData(['code' => '4','Title' => 'Canceled', 'Body' => $user_name.' canceled your request']);
+            $dataBuilder->addData(['code' => '4','Title' => 'Canceled', 'Body' => $user_name.' rejected your request.']);
             Log::info("Event Rjected:");
         }
         elseif($request_status == 'deleted'){

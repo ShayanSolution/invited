f3f36d7989dff5df827ec493025c971b2d64a75f
Dev3 <shayansolutions@gmail.com>
2018-07-18 11:16:08 +0500

    received request response update

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -220,6 +220,11 @@ class Event extends Model
         return $this->belongsTo('App\ContactList', 'list_id', 'id');
     }
 
+    public function contactListName()
+    {
+        return $this->belongsTo('App\ContactList', 'list_id', 'id')->select(['id', 'list_name']);
+    }
+
 
     protected function castAttribute($key, $value)
     {

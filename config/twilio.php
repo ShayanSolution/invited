5957231caa44feec31f6756092d0e639ae890210
Dev3 <shayansolutions@gmail.com>
2018-07-19 10:41:10 +0500

    updated twilio auth key

--- config/twilio.php
+++ config/twilio.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'accountId' => 'AC8bf700a1081c05d96be08ce0aeacccf3',
+    'accountId' => 'xxxxxxxxxxxxxx',
 //    'authKey' => 'a174861ea684bc1546523c6324d638d7',
-    'authKey' => '8398343eb767424db02f4d68f2455b2d',
-    'twilioNumber' => '+16162198881'
+    'authKey' => 'xxxxxxxxxxxx',
+    'twilioNumber' => 'xxxxxxxx'
 ];

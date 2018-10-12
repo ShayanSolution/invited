00038b9e473c936ef08bc8f96c7b1064ea440cd2
Dev3 <shayansolutions@gmail.com>
2018-10-12 19:17:26 +0500

    ios push notification config updated

--- config/push-notification.php
+++ config/push-notification.php
@@ -3,13 +3,13 @@
 return array(
 
     'invitedIOS'     => array(
-        'environment' =>'development',
+        'environment' =>'production',
         'certificate' =>base_path().env('INVITED_IOS_CERTIFICATE_PATH'),
         'passPhrase'  =>env('INVITED_IOS_PASS_PHRASE'),
         'service'     =>'apns'
     ),
     'appNameAndroid' => array(
-        'environment' =>'development',
+        'environment' =>'production',
         'apiKey'      =>'yourAPIKey',
         'service'     =>'gcm'
     )

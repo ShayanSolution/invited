46b336cb98b124fe6f009e1ff8d56f680a2fcedd
dev1 <fullstackkdev@gmail.com>
2018-11-09 15:08:24 +0500

    change environment pro to dev

--- config/push-notification.php
+++ config/push-notification.php
@@ -3,7 +3,8 @@
 return array(
 
     'invitedIOS'     => array(
-        'environment' =>'production',
+        //'environment' =>'production',
+        'environment' =>'development',
         'certificate' =>base_path().env('INVITED_IOS_CERTIFICATE_PATH'),
         'passPhrase'  =>env('INVITED_IOS_PASS_PHRASE'),
         'service'     =>'apns'

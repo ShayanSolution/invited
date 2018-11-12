614dc294133cc81c9d11194be29e809efc3bb53d
dev1 <fullstackkdev@gmail.com>
2018-11-12 18:36:14 +0500

    try and catch

--- app/Jobs/SendPushNotification.php
+++ app/Jobs/SendPushNotification.php
@@ -82,9 +82,9 @@ class SendPushNotification extends Job
 
         try {
             dd('helli');
-            Log::info("========================== In Try".$this->token."======================");
+            \Log::info("========================== In Try".$this->token."======================");
             // Validate the value...
-            $response = PushNotification::app('invitedIOS')->to($this->token)->send($message);
+            //$response = PushNotification::app('invitedIOS')->to($this->token)->send($message);
         } catch (\Exception $e) {
             Log::info("Notification response: ".$e);
             Log::info("Invalid device token.".$this->token);

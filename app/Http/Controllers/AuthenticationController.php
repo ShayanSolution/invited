f0ed5080b7d9f7d043646c726bdf974f3dc5a046
shayansolutions <shayansolutions@gmail.com>
2018-12-04 15:26:54 +0500

    email required for validation not working witout email

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -292,7 +292,7 @@ class AuthenticationController extends Controller
     public function registerValidation(Request $request){
         //dd($request);
         $validator = Validator::make($request->all(), [
-            //'email' => 'required|email|unique:users',
+            'email' => 'required|email|unique:users',
             'phone' => 'required|unique:users',
             'password' => 'required|min:6|confirmed',
             'password_confirmation' => 'required|min:6'

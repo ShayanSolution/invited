f6575b2ce1009adeaa85b579c0caf106fa409554
shayansolutions <shayansolutions@gmail.com>
2018-12-17 20:35:35 +0500

    Fix Name in send report pdf in v2.1

--- resources/views/sendReport/template.blade.php
+++ resources/views/sendReport/template.blade.php
@@ -17,52 +17,52 @@
 <table style="font-family: Arial">
     <tbody>
 
-        <tr>
-            <td><b>Event Type:</b>&nbsp;Send By Me</td>
-        </tr>
-        <tr>
-            <td><b>Event Title:</b>&nbsp;<?php echo $data['title']?></td>
-        </tr>
-        <tr>
-            <td><b>Event Location:</b>&nbsp;<?php echo $data['event_address']?></td>
-        </tr>
-        <tr>
-            <td><b>Event Sent Date:</b>&nbsp;<?php echo $date2?></td>
-        </tr>
-        <tr>
-            <td><b>Event Sent Time:</b>&nbsp;<?php echo $time2?></td>
-        </tr>
-        <tr>
-            <td><b>Event Date:</b>&nbsp;<?php echo $date?></td>
-        </tr>
-        <tr>
-            <td><b>Event Time:</b>&nbsp;<?php echo $time ?></td>
-        </tr>
-        <tr>
-            <td><b>List Name:</b>&nbsp;<?php echo $data['list_name']. ' ('.$listCount.')'?></td>
-        </tr>
-        <tr>
-            <td><b>Number of Invitation Accepted:</b>&nbsp;<?php echo $acceptedPeopelCount?></td>
-        </tr>
-        <tr>
-            <td><b>List of people who accept the invitation:</b></td>
-        </tr>
+    <tr>
+        <td><b>Event Type:</b>&nbsp;Send By Me</td>
+    </tr>
+    <tr>
+        <td><b>Event Title:</b>&nbsp;<?php echo $data['title']?></td>
+    </tr>
+    <tr>
+        <td><b>Event Location:</b>&nbsp;<?php echo $data['event_address']?></td>
+    </tr>
+    <tr>
+        <td><b>Event Sent Date:</b>&nbsp;<?php echo $date2?></td>
+    </tr>
+    <tr>
+        <td><b>Event Sent Time:</b>&nbsp;<?php echo $time2?></td>
+    </tr>
+    <tr>
+        <td><b>Event Date:</b>&nbsp;<?php echo $date?></td>
+    </tr>
+    <tr>
+        <td><b>Event Time:</b>&nbsp;<?php echo $time ?></td>
+    </tr>
+    <tr>
+        <td><b>List Name:</b>&nbsp;<?php echo $data['list_name']. ' ('.$listCount.')'?></td>
+    </tr>
+    <tr>
+        <td><b>Number of Invitation Accepted:</b>&nbsp;<?php echo $acceptedPeopelCount?></td>
+    </tr>
+    <tr>
+        <td><b>List of people who accept the invitation:</b></td>
+    </tr>
     </tbody>
 </table>
 
 <table width="400" >
     <tbody >
-        <tr >
-            <th align="left"><b>Name</b></th>
-            <th align="left"><b>Phone</b></th>
-        </tr>
+    <tr >
+        <th align="left"><b>Name</b></th>
+        <th align="left"><b>Phone</b></th>
+    </tr>
 
-        @foreach($filteredContacts as $contact)
-            <tr>
+    @foreach($contactListPersonName as $contact)
+        <tr>
             <td> {!! $contact['name'] or '' !!} </td>
             <td> {!! $contact['phone'] !!} </td>
-            </tr>
-        @endforeach
+        </tr>
+    @endforeach
     </tbody>
 </table>
 </html>

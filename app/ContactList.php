c099f7e2690a8d2b88ed78ca10ecd67b6f15ec98
shayansolutions <shayansolutions@gmail.com>
2018-12-18 16:17:14 +0500

    contactlist softdelete

--- app/ContactList.php
+++ app/ContactList.php
@@ -4,9 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Log;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ContactList extends Model
 {
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
     /**
      * The database table used by the model.
      *

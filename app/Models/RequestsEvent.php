3a2fcea35f564ef37b7300fdd8f5142cb335e539
shayansolutions <shayansolutions@gmail.com>
2018-12-31 17:28:34 +0500

    invitedBy name firstName and last name

--- app/Models/RequestsEvent.php
+++ app/Models/RequestsEvent.php
@@ -65,6 +65,7 @@ class RequestsEvent extends Model
                    $request_count[$index]['total'] = $request->total;
                    $request_count[$index]['create_by'] = $created_by->email;
                    $request_count[$index]['phone'] = $created_by->phone;
+                   $request_count[$index]['invited_by'] = $created_by->firstName.' '.$created_by->lastName;
                    $request_count[$index]['mobile'] = $created_by->mobile;
                    $request_count[$index]['address'] = $event->event_address;
                    $request_count[$index]['event_time'] = $event->event_time;

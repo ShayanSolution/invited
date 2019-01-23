c2ab8d16840ee8dc168a261ccbced4b14286d180
shayansolutions <shayansolutions@gmail.com>
2019-01-23 18:12:06 +0500

    accept enviro set for ios device

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -478,11 +478,11 @@ class EventController extends Controller
                     }
 //                    PushNotification::app('invitedIOS')->to($notification_user->device_token)->send($message);
                     if($notification_user->environment == 'development') {
-                        Log::info(" Environment is Development-----".$notification_user->device_token."---- Before Send and Environment:-----".$this->environment);
+                        Log::info(" Environment is Development-----".$notification_user->device_token."---- Before Send and Environment:-----".$notification_user->environment);
                         $response = PushNotification::app('invitedIOS')->to($notification_user->device_token)->send($message);
                         Log::info(" Environment is Development-----".$notification_user->device_token."------After Send");
                     } else{
-                        Log::info(" Environment is Production-----".$notification_user->device_token."---- Before Send and Environment:-----".$this->environment);
+                        Log::info(" Environment is Production-----".$notification_user->device_token."---- Before Send and Environment:-----".$notification_user->environment);
                         $response = PushNotification::app('invitedIOS')->to($notification_user->device_token)->send($message);
                         Log::info(" Environment is Production-----".$notification_user->device_token."------After Send");
                     }

d9bc7a335fdd89677cc09e5327c3e3d366dcf5de
shayansolutions <shayansolutions@gmail.com>
2019-01-23 17:32:02 +0500

    fix duplicate as well as noti on accept environment

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -478,11 +478,11 @@ class EventController extends Controller
 //                    PushNotification::app('invitedIOS')->to($notification_user->device_token)->send($message);
                     if($environment == 'development') {
                         Log::info(" Environment is Development-----".$this->token."---- Before Send and Environment:-----".$this->environment);
-                        $response = PushNotification::app('invitedIOSDev')->to($this->token)->send($message);
+                        $response = PushNotification::app('invitedIOS')->to($notification_user->device_token)->send($message);
                         Log::info(" Environment is Development-----".$this->token."------After Send");
                     } else{
                         Log::info(" Environment is Production-----".$this->token."---- Before Send and Environment:-----".$this->environment);
-                        $response = PushNotification::app('invitedIOS')->to($this->token)->send($message);
+                        $response = PushNotification::app('invitedIOS')->to($notification_user->device_token)->send($message);
                         Log::info(" Environment is Production-----".$this->token."------After Send");
                     }
                 }

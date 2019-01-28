d7f7999424b1486675448246f1ac1ccad44a5d9a
shayansolutions <shayansolutions@gmail.com>
2019-01-28 19:18:18 +0500

    Too late notiiii fix on master set env

--- app/Jobs/SendCloseEventNotification.php
+++ app/Jobs/SendCloseEventNotification.php
@@ -20,8 +20,9 @@ class SendCloseEventNotification extends Job
     protected $token;
     protected $event_title;
     protected $platform;
-    
-    
+    protected $environment;
+
+
     public function __construct($token,$event_title,$platform,$environment)
     {
         $this->token = $token;
@@ -39,7 +40,7 @@ class SendCloseEventNotification extends Job
     {
         Log::info(" I am in queue ");
         if($this->platform == 'ios' || is_null($this->platform)) {
-            Log::info(" I am in queue ios platform");
+            Log::info(" I am in queue ios platform".$this->environment);
             $message = PushNotification::Message("Too late. ".$this->event_title . "  has been closed ", array(
                 'badge' => 1,
                 'sound' => 'example.aiff',
@@ -56,7 +57,7 @@ class SendCloseEventNotification extends Job
                 ))
 
             ));
-//            PushNotification::app('invitedIOS')->to($this->token)->send($message);
+            //            PushNotification::app('invitedIOS')->to($this->token)->send($message);
             if($this->environment == 'development') {
                 Log::info(" Environment is Development-----".$this->token."---- Before Send and Environment:-----".$this->environment);
                 $response = PushNotification::app('invitedIOSDev')->to($this->token)->send($message);
@@ -70,19 +71,20 @@ class SendCloseEventNotification extends Job
         else{
             Log::info(" I am in queue ios android");
             $request_status = "TooLate";
-           // Log::info(" Send notification to android users ");
+            // Log::info(" Send notification to android users ");
             $optionBuilder = new OptionsBuilder();
             $optionBuilder->setTimeToLive(60*20);
             $dataBuilder = new PayloadDataBuilder();
             if($request_status == "TooLate"){
                 //$notificationBuilder = new PayloadNotificationBuilder('Accepted');
-                //$notificationBuilder->setBody($user_name.' accepted your request')->setSound('default');
-//            $dataBuilder->addData(['code' => '3','Title' => 'Accepted','Body' => $user_name.' confirmed your request.']);
+                //$notificationBuilder->setBody($user_name.' accepted your request')->setSound
+                ('default');
+//            $dataBuilder->addData(['code' => '3','Title' => 'Accepted','Body' => $user_name.'confirmed your request.']);
                 $dataBuilder->addData(['code' => '7','Title' => 'Accepted','Body' => 'Too late. '.$this->event_title.' has been closed']);
                 Log::info("Event Too Late:");
             }
 //            $notificationBuilder = new PayloadNotificationBuilder('Event Closed');
-//            $notificationBuilder->setBody('Too late. '.$this->event_title.' has been closed')->setSound('default');
+//            $notificationBuilder->setBody('Too late. '.$this->event_title.' has beenclosed')->setSound('default');
 //
 //
 //            $dataBuilder->addData(['a_data' => 'my_data']);

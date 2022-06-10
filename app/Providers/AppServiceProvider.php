2685a9ee0dc627fc677a8fe5ce644b27490f6ac7
Arslan Ali <marslan.ali@gmail.com>
2022-06-10 18:23:46 +0500

    error_reporting in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        if (config('app.debug')) {
+            error_reporting(E_ALL & ~E_USER_DEPRECATED);
+        } else {
+            error_reporting(0);
+        }
         $this->app->bind(\Illuminate\Contracts\Http\Kernel::class);
     }
 }
